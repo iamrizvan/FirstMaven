a1f5cc65f5fc777b7fb916418e76d634a1ea0f18
iamrizvan <rzkn000@gmail.com>
2020-05-01 20:19:09 +0400

    First maven cimmit

---
+++ src/main/java/com/riz/dev/spring/FirstMavenApp/App.java
@@ -0,0 +1,14 @@
+package com.riz.dev.spring.FirstMavenApp;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	for(int i=0;i<10;i++)
+    	{
+    		System.out.println(i);
+    	}
+    	System.out.println( "Hello World!" );
+        
+    }
+}
